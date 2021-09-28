2c3d806ecf379fd05bcd20808693ac363775c32b
tiankongguangzi@gmail.com <tiankongguangzi@gmail.com>
2021-09-28 18:24:53 +0200

    first setup

---
+++ README.md
@@ -0,0 +1,9 @@
+Data points to store
+
+1. Audio guide’s serial number
+2. Audio guide’s using record (how many times used)
+3. Triggered zone numbers
+4. Each triggered message’s listening percentage（25%，50%，75%，100%）
+5. Total messages inside audio guide
+6. Triggering way(IR or rfid)
+7. Language used
---
+++ migrations/20210928161936_test_migration/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `location` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Event` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
